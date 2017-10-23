45e97a5b4e66a19cff525e386f55389154f6c739
Victoria <vickyespinosa95@gmail.com>
2017-10-22 20:31:54 -0500

    keepmesafe Web

---
+++ firease/keepmesafeweb/functions/index.js
@@ -0,0 +1,38 @@
+const accountSid = 'AC189fad9eef9a38b7c137caa7a4b9273b';
+const authToken = 'a5cc77d84f5b391b152f79c24a07a082';
+
+const functions = require('firebase-functions');
+const twilio = require('twilio')(accountSid, authToken);
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+// AppiKey = AIzaSyBzxR-VqlkKffBH5Ojtq37NHNfUhu2tuZc
+
+
+exports.sanitize = functions.database
+    .ref('/users/{userId}/emergency/status')
+    .onWrite(event => {
+        const status = event.data.val();
+        console.log (status);
+        if(status === true){
+        	twilio.messages
+			  .create({
+			    to: '+15558675309',
+			    from: '+15017250604',
+			    body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+			  })
+			  .then((message) => console.log(message.sid));
+        }
+        
+        return event.data.ref.set(question);
+});
+
+sanitize = s => {
+    let sanitatizeText = s;
+    sanitatizeText = sanitatizeText.replace(/\bstupid\b/ig, "great");
+    return sanitatizeText;
+}
